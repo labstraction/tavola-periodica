701317fcaeb4eef290f4acd11b6c045ef3169bb8
labstraction <45163263+labstraction@users.noreply.github.com>
2024-11-14 11:42:19 +0100

    javascript

---
+++ programma.js
@@ -0,0 +1,16 @@
+
+
+let contenitore = document.getElementById('contenitore');
+
+contenitore.style.borderStyle = 'solid'
+
+let cella = document.createElement('div');
+
+let numeroAtomico = document.createElement('p');
+numeroAtomico.innerText = '1';
+
+let simbolo = document.createElement('a');
+simbolo.innerText = 'H';
+
+let nome = document.createElement('p');
+nome.innerText = 'Idrogeno';
