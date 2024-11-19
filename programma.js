3205dc83470df21e3f97162bcdb0095cdbdee5b0
labstraction <45163263+labstraction@users.noreply.github.com>
2024-11-19 12:40:29 +0100

    super tavola

--- programma.js
+++ programma.js
@@ -1,37 +1,36 @@
 
-let idrogeno = {};
-
-idrogeno.simbolo = 'H';
-idrogeno.numero = 1;
-idrogeno.nome = 'Idrogeno';
-
-let elio = {};
-
-elio.simbolo = 'He';
-elio.numero = 2;
-elio.nome = 'Elio';
-
-
-let elementi = [idrogeno, elio];
 
 for (const element of elementi) {
     let contenitore = document.getElementById('contenitore');
+    contenitore.style.position = 'relative';
 
 
     let cella = document.createElement('div');
     cella.style.borderStyle = 'solid';
-    cella.style.width = '70px'
+    cella.style.borderWidth = '0.5px';
+    cella.style.width = '90px';
+    cella.style.height = '120px';
+    cella.style.position = 'absolute';
+    cella.style.left = element.xpos * 90 + 'px';
+    cella.style.top = element.ypos * 120 + 'px';
+    cella.style.backgroundColor = '#' + element['cpk-hex'];
+    cella.style.textAlign = 'center';
+    cella.style.boxSizing = 'border-box';
 
     let numeroAtomico = document.createElement('p');
-    numeroAtomico.innerText = element.numero;
+    numeroAtomico.innerText = element.number;
     cella.appendChild(numeroAtomico);
 
     let simbolo = document.createElement('a');
-    simbolo.innerText = element.simbolo;
+    simbolo.innerText = element.symbol;
+    simbolo.href = element.source;
     cella.appendChild(simbolo);
+    simbolo.style.fontSize = '24px';
+    simbolo.style.fontWeight = 'bold';
+    simbolo.style.color = 'black';
 
     let nome = document.createElement('p');
-    nome.innerText = element.nome;
+    nome.innerText = element.name;
     cella.appendChild(nome);
 
     contenitore.appendChild(cella);
